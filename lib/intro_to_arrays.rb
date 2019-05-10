9698d6cfd0a4b0a5e6e86fffeb23e8de83253fb0
johncban <johncban@gmail.com>
2019-05-10 22:17:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,4 +6,7 @@ def array_with_two_elements
   ar = [1, 2]
 end
 
-def
+def first_element
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  puts "#{taylor_swift[1]}"
+end
